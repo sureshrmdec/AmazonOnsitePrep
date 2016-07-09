ad7ddd0edc4027f2d1a30e8b5e17ed87c305ef85
Hanyuan Zhang <shalltalk@users.noreply.github.com>
2016-07-08 17:02:39 -0700

    edited the problem

--- DrawLine.java
+++ DrawLine.java
@@ -6,7 +6,8 @@ public class DrawLine {
 	 * consecutive pixels to be stored in one byte. The screen has width w,
 	 * where w is divisible by 8 (that is, no byte will be split across rows).
 	 * The height of the screen, of course, can be derived from the length of
-	 * the array and the width. Implement a function that draws a horizontal
+	 * the array and the width because the length = height x width. 
+	 * Implement a function that draws a horizontal
 	 * line from (x1, y) to (x2, y).
 	 * 
 	 * The method signature should look something like:
